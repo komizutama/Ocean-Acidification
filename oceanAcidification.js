ce32dc962b4eb19e39e16e6ff827962cac1d5bca
Matthias Elliott <komizutama@gmail.com>
2016-10-31 18:12:04 -0400

    addedcollision registry and unitNormal and unitTangent calculations and began elastic collision function.

--- oceanAcidification.js
+++ oceanAcidification.js
@@ -1,6 +1,7 @@
 const numIons = 50;
 var molecules = [];
 var bouncey = true;
+var collisionLog = [];
 
 // http://paletton.com/#uid=73v1c0kllll0W7ob8envysjYfES
 
@@ -14,24 +15,45 @@ function setup() {
 function draw() {
   var backgroundColor = color(75, 75, 78);
   background(backgroundColor);
+  movementHandler()
+}
+
+function movementHandler () {
   for (var i=0; i < molecules.length; i++) {
     molecules[i].checkEdges(bouncey);
-    console.log(molecules[i].checkCollision(i))
+    // molecules[i].elasticCollision(i);
     molecules[i].move();
     // molecules[i].displayDot();
     molecules[i].displayECloud();
   }
+  collisionLog.fill(false);
 }
 
 function reset(){
   for (var i=0; i < numIons; i++) {
     molecules.push(new ion());
+    collisionLog.push(false);
   }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
+
+function unitNormal (posA, posB) {
+  var normalVec = [(posB[0]-posA[0]), (posB[1]-posA[1])];
+  var unitNormal=[];
+  for (var i=0; i < normalVec.length; i++){
+    unitNormal.push(normalVec[i]/Math.sqrt(Math.pow(unitNormal[0],2)+Math.pow(unitNormal[1], 2)));
+  }
+  return unitNormal;
+}
+
+function unitTan (unitNormal) {
+  var unitTan = [-unitNormal[1], unitNormal[0]];
+  return unitTan;
+}
+
 
 function wallWrap(pos, trav, axisSize) {
   if (pos + trav > axisSize) {
@@ -44,6 +66,8 @@ function wallWrap(pos, trav, axisSize) {
     return pos + trav;
   }
 }
+
+
 
 function wallBounce(pos, trav, axisSize, diam){
   if (pos + trav + diam/2> axisSize) {
@@ -64,7 +88,7 @@ function ion() {
   this.speed = 1;
   this.ionColor = color(255, 142, 0);
   this.position = [random(width-this.diameter/2)+this.diameter/2, random(height-this.diameter/2)+this.diameter/2];
-  this.delta = [random(-this.speed, this.speed), random(-this.speed, this.speed)];
+  this.velocity = [random(-this.speed, this.speed), random(-this.speed, this.speed)];
   this.canvasSize = [windowWidth, windowHeight];
 
 
@@ -72,32 +96,41 @@ function ion() {
   this.checkEdges = function(b) {
     if (b) {
       for (var i=0; i < this.position.length; i++) {
-        this.delta[i] = wallBounce(this.position[i], this.delta[i], this.canvasSize[i], this.diameter);
+        this.velocity[i] = wallBounce(this.position[i], this.velocity[i], this.canvasSize[i], this.diameter);
       }
     }
     else {
       for (var i=0; i < this.position.length; i++) {
-        this.position[i] = wallWrap(this.position[i], this.delta[i], this.canvasSize[i]);
+        this.position[i] = wallWrap(this.position[i], this.velocity[i], this.canvasSize[i]);
       }
     }
   }
 
-  this.checkCollision = function(i) {
-    for (var j = i+1; j < molecules.length; j++) {
-      var dx = Math.abs(molecules[i].position[0])-Math.abs(molecules[j].position[0]);
-      var dy = Math.abs(molecules[i].position[1])-Math.abs(molecules[j].position[1]);
-      var distance = Math.sqrt(dx * dx + dy * dy)
 
-      if (distance < molecules[i].diameter + molecules[j].diameter) {
-        return true;
+  this.collisionHandler = function(i) {
+    if (collisionLog[i] === false){
+      for (var j = i+1; j < molecules.length; j++) {
+        var dx = Math.abs(molecules[i].position[0])-Math.abs(molecules[j].position[0]);
+        var dy = Math.abs(molecules[i].position[1])-Math.abs(molecules[j].position[1]);
+        var distance = Math.sqrt(dx * dx + dy * dy)
+        if (distance < molecules[i].diameter + molecules[j].diameter) {
+          collisionLog[i] = true;
+          collisionLog[j] = true;
+          this.elasticCollision[this.molecules[i], this.molecules[j]]
+        }
       }
     }  
+  }
+
+  this.elasticCollision = function (molA, MolB) {
+    unitNormal= unitTan(molA.position, molB.position);
+    unitTan(unitNormal);
   }
 
 // change location
   this.move = function() {
     for (var i=0; i < this.position.length; i++){
-      this.position[i] += this.delta[i];
+      this.position[i] += this.velocity[i];
     }
   }
 
